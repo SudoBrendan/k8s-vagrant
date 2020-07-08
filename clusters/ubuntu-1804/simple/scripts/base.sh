bde1fc75a4bf4a71cdefb93669acd9d0dbd5bee8
Brendan Bergen <brendandbergen@gmail.com>
2020-07-08 16:31:39 -0500

    No hard-coded domains.

--- clusters/ubuntu-1804/simple/scripts/base.sh
+++ clusters/ubuntu-1804/simple/scripts/base.sh
@@ -4,12 +4,12 @@ echo "=============================="
 echo "HOSTS FILE"
 echo "=============================="
 cat >>/etc/hosts<<EOF
-10.0.1.10 ${PREFIX}-master0.example.com ${PREFIX}-master0
-10.0.1.20 ${PREFIX}-worker0.example.com ${PREFIX}-worker0
-10.0.1.21 ${PREFIX}-worker1.example.com ${PREFIX}-worker1
-10.0.1.22 ${PREFIX}-worker2.example.com ${PREFIX}-worker2
-10.0.1.23 ${PREFIX}-worker3.example.com ${PREFIX}-worker3
-10.0.1.24 ${PREFIX}-worker4.example.com ${PREFIX}-worker4
+10.0.1.10 ${PREFIX}-master0.${DOMAIN} ${PREFIX}-master0
+10.0.1.20 ${PREFIX}-worker0.${DOMAIN} ${PREFIX}-worker0
+10.0.1.21 ${PREFIX}-worker1.${DOMAIN} ${PREFIX}-worker1
+10.0.1.22 ${PREFIX}-worker2.${DOMAIN} ${PREFIX}-worker2
+10.0.1.23 ${PREFIX}-worker3.${DOMAIN} ${PREFIX}-worker3
+10.0.1.24 ${PREFIX}-worker4.${DOMAIN} ${PREFIX}-worker4
 EOF
 
 echo "[TASK] Enable ssh password authentication"
